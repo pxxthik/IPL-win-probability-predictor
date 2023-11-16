987e3e2f150ca7638832f41cb4c93b8f1833f7c9
Pratheek Bedre <pratheekbedrejun20@gmail.com>
2023-11-16 10:04:06 +0530

    bug fix

--- app.py
+++ app.py
@@ -41,16 +41,19 @@ with col5:
     wickets = st.number_input('Wickets out')
 
 if st.button("Predict Probability"):
-    runs_left = target - score
-    balls_left = 120 - (overs*6)
-    wickets = 10 - wickets
-    crr = score/overs
-    rrr = (runs_left*6)/balls_left
+    if overs > 0:
+        runs_left = target - score
+        balls_left = 120 - (overs*6)
+        wickets = 10 - wickets
+        crr = score/overs
+        rrr = (runs_left*6)/balls_left
 
-    input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
+        input_df = pd.DataFrame({'batting_team':[batting_team],'bowling_team':[bowling_team],'city':[selected_city],'runs_left':[runs_left],'balls_left':[balls_left],'wickets':[wickets],'total_runs_x':[target],'crr':[crr],'rrr':[rrr]})
 
-    result = pipe.predict_proba(input_df)
-    loss = result[0][0]
-    win = result[0][1]
-    st.header(batting_team + " - " + str(round(win * 100)) + "%")
-    st.header(bowling_team + " - " + str(round(loss * 100)) + "%")
+        result = pipe.predict_proba(input_df)
+        loss = result[0][0]
+        win = result[0][1]
+        st.header(batting_team + " - " + str(round(win * 100)) + "%")
+        st.header(bowling_team + " - " + str(round(loss * 100)) + "%")
+    else:
+        st.text("Select overs completed")
